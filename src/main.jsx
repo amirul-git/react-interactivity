d59218eeef04de46b74e44f124146b378c72f66a
amir-repository <achmadnassardhi@gmail.com>
2023-08-29 03:44:44 +0700

    Add react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import Root from "./routes/root";
+import ErrorPage from "./routes/error-page";
+import Light from "./routes/light";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/light",
+    element: <Light />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
---
+++ src/routes/root.jsx
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+
+export default function Root() {
+  return (
+    <>
+      <main className="p-6 flex justify-center   gap-3">
+        <Link
+          to={"light"}
+          className="cursor-pointer px-4 py-2 bg-white rounded-lg"
+        >
+          🌞 Light
+        </Link>
+        <div className="cursor-pointer px-4 py-2 bg-white rounded-lg">
+          🏖 Sandbox
+        </div>
+        <div className="cursor-pointer px-4 py-2 bg-white rounded-lg">
+          📊 Sales
+        </div>
+        <div className="cursor-pointer px-4 py-2 bg-white rounded-lg">
+          🔍 Search
+        </div>
+        <div className="cursor-pointer px-4 py-2 bg-white rounded-lg">
+          ⏳Filter
+        </div>
+      </main>
+    </>
+  );
+}
